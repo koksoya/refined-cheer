d36ccda3bf9b52a33cc0e1c8138137b80ffdceee
koksoya <b.koksoya@gmail.com>
2019-05-13 01:05:27 +0300

    minor refactor on app.js

--- app.js
+++ app.js
@@ -3,8 +3,5 @@ const cron = require("node-cron")
 const express = require("express")
 
 const app = express();
-const port = 3000
 
 cron.schedule("33 3 * * *", () => index())
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
